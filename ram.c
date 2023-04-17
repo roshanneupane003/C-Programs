fd30b047eab8010d46ad93b2b1471651b12158b9
roshanneupane003 <130978801+roshanneupane003@users.noreply.github.com>
2023-04-17 15:58:31 +0545

    Add files via upload

---
+++ ram.c
@@ -0,0 +1,25 @@
+#include<stdio.h0>
+int main()
+{
+ int a,b;
+ char operator;
+ printf("enter any two integers")
+ scanf("%d %d",&a,&b);
+ printf("enter an operator");
+ scanf("%c",operator);
+switch(operator)
+{
+  case'-':
+  printf("the substraction is %d",a-b);
+  break;
+
+  case'+':
+  printf ("the sum is %d",a+b);
+  break;
+
+  default:
+  printf("you entered a wrong operator");
+
+}
+
+}
